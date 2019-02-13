1e44bb8930249221eaf52bf931dfccaddf793574
Adi Binenbaum <adi.binen@gmail.com>
2019-02-13 16:10:00 +0200

    circles

--- js/main.js
+++ js/main.js
@@ -1,40 +1,51 @@
 'use strict'
 
-var gCanvas
-var gCtx
+var canvas
+var ctx
+var isDrawing = false;
+var points = [];
+var lastX = 0;
+var lastY = 0;
+
 
 function init() {
-    gCanvas = document.querySelector('#canvas');
-    gCtx = gCanvas.getContext('2d');
-    console.log(gCtx);
-
-    gCtx.lineJoin = 'round';
-    gCtx.lineCap = 'round';
-    gCtx.lineWidth = 20;
-    gCtx.strokeStyle = "#000000";
-
-    let isDrawing = false;
-    let lastX = 0;
-    let lastY = 0;
-
-    function draw(e) {
-        // stop the function if they are not mouse down
-        if (!isDrawing) return;
-        //listen for mouse move event
-        console.log(e);
-        gCtx.beginPath();
-        gCtx.moveTo(lastX, lastY);
-        gCtx.lineTo(e.offsetX, e.offsetY);
-        gCtx.stroke();
-        [lastX, lastY] = [e.offsetX, e.offsetY];
-    }
-
-    gCanvas.addEventListener('mousedown', (e) => {
+    canvas = document.querySelector('#canvas');
+    ctx = canvas.getContext('2d');
+    canvas.addEventListener('mousedown', (e) => {
         isDrawing = true;
+        points.push({ x: e.clientX, y: e.clientY });
         [lastX, lastY] = [e.offsetX, e.offsetY];
     });
-    gCanvas.addEventListener('mousemove', draw);
-    gCanvas.addEventListener('mouseup', () => isDrawing = false);
-    gCanvas.addEventListener('mouseout', () => isDrawing = false);
+    canvas.addEventListener('mousemove', draw);
+    canvas.addEventListener('mouseup', () => {
+        isDrawing = false;
+        points.length = 0;
+    });
+    canvas.addEventListener('mouseout', () => isDrawing = false);
+}
 
+function draw(e) {
+    // stop the function if they are not mouse down
+    if (!isDrawing) return;
+    
+
+    points.push({ x: e.clientX, y: e.clientY });
+    for (var i = 0; i < points.length; i++) {
+        ctx.beginPath();
+        drawCircles(i);
+        
+        ctx.fill();
+        ctx.stroke();
+    }
+}
+
+function drawCircles(i) {
+    ctx.lineJoin = ctx.lineCap = 'round';
+    
+    ctx.arc(points[i].x, points[i].y, 15, false, Math.PI * 2, false);
+}
+
+function drawColor() {
+    let color = document.querySelector('input').value;
+    ctx.fillStyle = color;
 }
