a07d9fc63d9807af08e09103cf971a61805d4ede
Adi Binenbaum <adi.binen@gmail.com>
2019-02-13 18:00:32 +0200

    before css

--- js/main.js
+++ js/main.js
@@ -10,6 +10,7 @@ var lastY = 0;
 function init() {
     canvas = document.querySelector('#canvas');
     ctx = canvas.getContext('2d');
+    resizeCanvas();
     canvas.addEventListener('mousedown', (e) => {
         isDrawing = true;
         points.push({ x: e.clientX, y: e.clientY });
@@ -92,3 +93,48 @@ function drawColor() {
     let color = document.querySelector('input').value;
     ctx.strokeStyle = color;
 }
+
+//download img
+function downloadImg(elLink) {
+    var imgContent = canvas.toDataURL('image/png');
+    elLink.href = imgContent
+}
+
+function resizeCanvas() {
+    var elContainer = document.querySelector('.canvas-container');
+    canvas.width = elContainer.offsetWidth
+    canvas.height = elContainer.offsetHeight
+ }
+ 
+ function onFileInputChange(ev) {
+    handleImageFromInput(ev, renderCanvas)
+ }
+ 
+ //UPLOAD IMG WITH INPUT FILE
+ function handleImageFromInput(ev, onImageReady) {
+    document.querySelector('.share-container').innerHTML = ''
+    var reader = new FileReader();
+    console.log(ev)
+    reader.onload = function (event) {
+        var img = new Image();
+        img.onload = onImageReady.bind(null, img)
+        img.src = event.target.result;
+    }
+    reader.readAsDataURL(ev.target.files[0]);
+ }
+ 
+ function renderCanvas(img) {
+    canvas.width = img.width;
+    canvas.height = img.height;
+    ctx.drawImage(img, 0, 0);
+ }
+
+
+// facebook api
+// (function(d, s, id) {
+//     var js, fjs = d.getElementsByTagName(s)[0];
+//     if (d.getElementById(id)) return;
+//     js = d.createElement(s); js.id = id;
+//     js.src = 'https://connect.facebook.net/he_IL/sdk.js#xfbml=1&version=v3.0&appId=807866106076694&autoLogAppEvents=1';
+//     fjs.parentNode.insertBefore(js, fjs);
+//   }(document, 'script', 'facebook-jssdk'));
